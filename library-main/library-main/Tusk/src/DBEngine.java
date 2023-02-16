46f8044c3f6891726cf62b8d71bf3fa50561fcde
1nurbakyt1 <125080236+1nurbakyt1@users.noreply.github.com>
2023-02-16 13:24:34 +0600

    Delete DBEngine.java

--- library-main/library-main/Tusk/src/DBEngine.java
+++
@@ -1,6 +0,0 @@
-
-public class DBEngine{
-    public static final String url = "jdbc:mysql://localhost:3306/libra ry";
-    public static final String user = "root";
-    public static final String password = "12345";
-}
